ee919cf149c211ec67e20710cc97eab239c59f51
crycetruly <crycetruly@gmail.com>
2019-07-15 23:01:18 +0300

    ft(notes): Adapt data to views

---
+++ app/src/main/java/com/crycetruly/notekeeper/NoteListActivity.kt
@@ -0,0 +1,43 @@
+package com.crycetruly.notekeeper
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.ArrayAdapter
+import androidx.appcompat.app.AppCompatActivity;
+import com.crycetruly.notekeeper.model.NoteInfo
+
+import kotlinx.android.synthetic.main.activity_note_list.*
+import kotlinx.android.synthetic.main.content_note_list.*
+
+class NoteListActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_note_list)
+
+        supportActionBar?.setTitle("Notes")
+
+        fab.setOnClickListener { view ->
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+
+        val adapter = ArrayAdapter<NoteInfo>(this, android.R.layout.simple_list_item_1, DataManager.notes)
+        noteList.adapter = adapter
+
+        noteList.setOnItemClickListener { adapterView, view, i, l ->
+            val intent = Intent(baseContext, MainActivity::class.java)
+            intent.putExtra(POSITION, i)
+            startActivity(
+                intent
+            )
+        }
+    }
+
+    override fun onResume() {
+        (noteList.adapter as ArrayAdapter<NoteInfo>).notifyDataSetChanged()
+        super.onResume()
+    }
+
+
+}
